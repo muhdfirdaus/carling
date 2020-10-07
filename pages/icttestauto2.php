d1b80cfc849e0558a228d67b09009ee500fa9ec0
muhdfirdaus <dausmomo5779@gmail.com>
2020-10-07 10:42:04 +0800

    ICT log update now split into two files to avoid missing data

--- pages/icttestauto2.php
+++ pages/icttestauto2.php
@@ -1,7 +1,7 @@
 
 <h1>Don't close this page!</h1>
 <pre>
-This page is set to update logfile for Carling(1/2) packing system automatically.
+This page is set to update logfile for Carling(2/2) packing system automatically.
 </pre>
 
 <?php 
@@ -9,15 +9,9 @@ This page is set to update logfile for Carling(1/2) packing system automatically
 include('../dist/includes/dbcon.php');
 
 
-$query=mysqli_query($con,"select lastupdate from ict_test order by lastupdate desc limit 1")or die(mysqli_error($con));
-$row=mysqli_fetch_array($query);
-$lupdICT=$row['lastupdate'];
-// $lupdICT = 1600620271;
 
-$query=mysqli_query($con,"select lastupdate from sn_panel order by lastupdate desc limit 1")or die(mysqli_error($con));
-$row=mysqli_fetch_array($query);
-$lupdPanel=$row['lastupdate'];
-// $lupdPanel = 1600361433;
+$lupdICT=$_GET['lastupdate'];
+// $lupdICT = 1600620271;
 
 
 // ***********************************  Fetching ICT Log  **************************************************************
@@ -25,11 +19,12 @@ $lupdPanel=$row['lastupdate'];
 
 $mydir = "\\\\10.38.30.173\\Test_log";// Check if ICT sharefolder is accessible
 if(file_exists($mydir)){
-    $dir    = '\\\\10.38.30.173\\Test_log\\ICT\\942-10048';
+
+    $dir    = '\\\\10.38.30.173\\Test_log\\ICT\\942-10047';
     $files1 = scandir($dir);
 
     foreach($files1 as $file){
-        if(date (filemtime("\\\\10.38.30.173\\Test_log\\ICT\\942-10048\\".$file))>$lupdICT){
+        if(date (filemtime("\\\\10.38.30.173\\Test_log\\ICT\\942-10047\\".$file))>$lupdICT){
         // if((filemtime("C:/carling/ict/".$file))){    
             if(strlen($file)>5){
                 if(strpos($file, 'default') !== false){
@@ -40,7 +35,42 @@ if(file_exists($mydir)){
                     $panel = preg_replace('/\s+/', '', $full[0]);
                     $res = $full[2][0];
 
-                    $fdate = date (filemtime("\\\\10.38.30.173\\Test_log\\ICT\\942-10048\\".$file));
+                    $fdate = date (filemtime("\\\\10.38.30.173\\Test_log\\ICT\\942-10047\\".$file));
+                    
+                    set_time_limit(0);
+                    
+                    if(isset($data[$panel])){
+                        if($data[$panel]['d']<$fdate){
+                            $data[$panel]['r'] = $res;
+                            $data[$panel]['d'] = $fdate;
+                        }
+                    }
+                    else{
+                        $data[$panel]['p'] = $panel;
+                        $data[$panel]['r'] = $res;
+                        $data[$panel]['d'] = $fdate;
+                    }
+                }
+            }
+        }
+    }
+
+    $dir    = '\\\\10.38.30.173\\Test_log\\ICT\\942-10046';
+    $files1 = scandir($dir);
+
+    foreach($files1 as $file){
+        if(date (filemtime("\\\\10.38.30.173\\Test_log\\ICT\\942-10046\\".$file))>$lupdICT){
+        // if((filemtime("C:/carling/ict/".$file))){    
+            if(strlen($file)>5){
+                if(strpos($file, 'default') !== false){
+                    //Do nothing
+                }
+                else{
+                    $full = explode("-",$file);
+                    $panel = preg_replace('/\s+/', '', $full[0]);
+                    $res = $full[2][0];
+
+                    $fdate = date (filemtime("\\\\10.38.30.173\\Test_log\\ICT\\942-10046\\".$file));
                     
                     set_time_limit(0);
                     
@@ -86,28 +116,7 @@ if(isset($data)){
 }
 
 
-// ***********************************  Fetching SN-Panel  **************************************************************
-$dir    = 'C:\carling\panel_sn';
-$files1 = scandir($dir);
-foreach($files1 as $files){
-    if(strlen($files)>5){
-        if(date(filemtime("C:/carling/panel_sn/".$files))>$lupdPanel){
-        // if((filemtime("C:/carling/panel_sn/".$files)){
-            set_time_limit(0);
-            $file = fopen("C:/carling/panel_sn/$files","r");
-            if(fgetcsv($file) !== FALSE){
-                while (($line = fgetcsv($file)) !== FALSE) {
-                    $panel = preg_replace('/\s+/', '', $line[0]);
-                    $sn = preg_replace('/\s+/', '', $line[1]); 
-                    $updateon = time();
-                    mysqli_query($con, "INSERT INTO sn_panel (panel_no,sn, lastupdate) values('$panel','$sn', '$updateon')")or die(mysqli_error($con));
-                }
-            }
-            
-            fclose($file);
-        }
-    }
-}
-echo "<script>document.location='icttestauto2.php?lastupdate=$lupdICT'</script>";
+echo "Last Updated on: ".date('d-m-Y H:i:s', time());
 
+header( "refresh:3600;url=icttestauto.php" );
 ?>
